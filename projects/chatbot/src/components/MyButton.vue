5f3254d188ad41cee407a5be979c8261378398a0
Nick Schneeberger <nick.schneeberger@icloud.com>
2023-04-04 10:53:56 +0200

    g

---
+++ projects/chatbot/src/components/MyButton.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+      <!-- Your template code goes here -->
+    </div>
+
+</template>
+  
+<script>
+
+  export default {
+    name: 'MyButton',
+    data() {
+      return {
+        // Your data properties go here
+      };
+    },
+    methods: {
+      // Your methods go here
+    }
+  };
+
+</script>
+
+<style>
+</style>
---
+++ projects/chatbot/src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+
+  <h1> Das ist mein Vue Projekt </h1>
+
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'HomeView',
+  components: {
+    
+  }
+}
+
+</script>
